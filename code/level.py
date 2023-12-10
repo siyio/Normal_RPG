67915bb9dd79b0462037dda19f464aae244ad754
SiYi04 <siyi04@qq.com>
2023-12-10 18:03:03 +0800

    增加视角固定, 修改碰撞箱体积

--- code/level.py
+++ code/level.py
@@ -2,20 +2,41 @@ import pygame
 from setting import *
 from tile import Tile
 from player import Player
+from debug import debug
 class Level:
 	def __init__(self):
+		self.player=Player
 		self.display_surface = pygame.display.get_surface()
-		self.visible_sprites = pygame.sprite.Group()
+		self.visible_sprites = YSortCameraGroup()
 		self.obstacles_sprites = pygame.sprite.Group()
-
+		self.create_map()
 	def create_map(self):
 		for row_index,row in enumerate(WORLD_MAP):
 			for col_index, col in enumerate(row):
-				x=row_index * TILESIZE
-				y=col_index * TILESIZE
+				x=col_index * TILESIZE
+				y=row_index * TILESIZE
 				if col == 'x':
-					Tile((x,y),[self.visible_sprites])
+					Tile((x, y), [self.visible_sprites, self.obstacles_sprites])
 				if col == 'p':
-					Player((x,y),self.visible_sprites)
+					self.player = Player((x,y),[self.visible_sprites],self.obstacles_sprites)
+
 	def run(self):
-		self.visible_sprites.draw(self.display_surface)
+		self.visible_sprites.custom_draw(self.player)
+		self.visible_sprites.update()
+		# debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+	def __init__(self):
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.half_width = self.display_surface.get_size()[0] // 2
+		self.half_height = self.display_surface.get_size()[1] // 2
+		self.offset = pygame.math.Vector2()
+
+	def custom_draw(self, player):
+		self.offset.x = player.rect.centerx - self.half_width
+		self.offset.y = player.rect.centery - self.half_height
+
+		for sprite in self.sprites():
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image, offset_pos)
